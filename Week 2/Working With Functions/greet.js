bdccdea8b574ed00c6352730dcbdcdaafcf26e52
Nursyafira Jamil <32829952+fyra-jamil@users.noreply.github.com>
2021-05-31 14:44:42 +0800

    Update greet.js

--- Week 2/Working With Functions/greet.js
+++ Week 2/Working With Functions/greet.js
@@ -4,14 +4,13 @@ Write the greet() function
 - for ex. functionName(parameter){...}
 */
 function greet(name) {
-  //your code goes down here
   return 'Hello ' + name;
 }
 
 //open the browser console to check results
 console.log('results: ', greet('Dani'));
 
-//don't change this line
+
 if (typeof module !== 'undefined') {
   module.exports = greet;
 }
