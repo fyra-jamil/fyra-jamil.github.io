1f215ce6cfd89d99b7e2f16167abac30180412a4
Nursyafira Jamil <32829952+fyra-jamil@users.noreply.github.com>
2021-05-31 14:46:33 +0800

    Update variables01.js

--- Week 2/Working With Variable/variables01.js
+++ Week 2/Working With Variable/variables01.js
@@ -11,7 +11,7 @@ function greetings() {
 //leave this line unchanged to console log the results
 console.log('results: ', greetings());
 
-//don't change this line
+
 if (typeof module !== 'undefined') {
   module.exports = greetings;
 }
