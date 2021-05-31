1addc93120ae03fac6fa812d3a1d4ec8798799da
Nursyafira Jamil <32829952+fyra-jamil@users.noreply.github.com>
2021-05-31 14:50:13 +0800

    Update soccer-manager.js

--- Week 3/Equal Operators/soccer-manager.js
+++ Week 3/Equal Operators/soccer-manager.js
@@ -3,9 +3,6 @@
 
 //Run the code in the console to find out what's the secondTeam final playing style.
 
-/////////////////////////
-//your code here
-
 function getPlayingStyle(){
   let firstTeam = [3,3,1,3];
   let secondTeam = firstTeam;
@@ -15,15 +12,11 @@ function getPlayingStyle(){
   firstTeam[3]=1;
 
   return secondTeam;
-
 }
 
-/////////////////////////
 //Uncomment this line to see the results in the browser console
-//console.log("results = " + getPlayingStyle())
+console.log("results = " + getPlayingStyle())
 
-/////////////////////////
-//don't change the code under this line
 if (typeof module !== 'undefined') {
   module.exports = getPlayingStyle;
 }
