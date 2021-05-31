6844f730d19081b39a69ac21a7fcb913eb22de9d
Nursyafira Jamil <32829952+fyra-jamil@users.noreply.github.com>
2021-05-31 14:51:50 +0800

    Update scope.js

--- Week 3/Variable Scope/scope.js
+++ Week 3/Variable Scope/scope.js
@@ -1,7 +1,6 @@
 var animal = 'dog';
 
 function myPet() {
-  // You should not need to modify this function
   return animal;
 }
 
@@ -9,15 +8,13 @@ function yourPet() {
   // The expectation for this function is to return `animal` just like the previous function
   // However, you cannot simply modify the existing variable declared in the global scope
   // Note: writing return 'cat' below will not work
-  /////////////////////
-  //your code goes here
+  
   let animal ='cat';
-  ////////////////////
   return animal;
 }
 
 //open your browser console to check the results
 console.log('myPet result: ' + myPet());
 console.log('yourPet result: ' + yourPet());
-//Don't change this line
+
 module.exports = { myPet: myPet, yourPet: yourPet };
